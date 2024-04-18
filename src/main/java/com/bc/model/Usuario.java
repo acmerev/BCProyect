a904571c64a29f51705b10122fac96558bcf56f2
acmerev <acmerev@gmail.com>
2024-04-17 23:47:26 -0600

    Add Anotation @Data for reduce code

--- src/main/java/com/bc/model/Usuario.java
+++ src/main/java/com/bc/model/Usuario.java
@@ -1,5 +1,9 @@
 package com.bc.model;
 
+import lombok.Data;
+
+
+@Data
 public class Usuario {
 
     private String nombre;
@@ -7,15 +11,8 @@ public class Usuario {
     private int edad;
     private String email;
 
-    
-    @Override
-	public String toString() {
-		return "Usuario [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", email=" + email + "]";
-	}
-
 	public Usuario() {
 	}
-
 	public Usuario(String nombre, String apellido, int edad, String email) {
 		this.nombre = nombre;
 		this.apellido = apellido;
@@ -23,36 +20,4 @@ public class Usuario {
 		this.email = email;
 	}
 
-    // Getters y setters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
